ef5f47fa72dcecc931239207238464869e675d81
Tkachov Mykhailo <mtkachov435@gmail.com>
2024-08-16 20:27:26 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,47 @@
 from app.main import get_coin_combination
 
+import pytest
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "amount_to_convert,extend_value",
+    [
+        pytest.param(
+            0,
+            [0, 0, 0, 0],
+            id=""
+        ),
+        pytest.param(
+            4,
+            [4, 0, 0, 0],
+            id=""
+        ),
+        pytest.param(
+            5,
+            [0, 1, 0, 0],
+            id=""
+        ),
+        pytest.param(
+            9,
+            [4, 1, 0, 0],
+            id=""
+        ),
+        pytest.param(
+            10,
+            [0, 0, 1, 0],
+            id=""
+        ),
+        pytest.param(
+            24,
+            [4, 0, 2, 0],
+            id=""
+        ),
+        pytest.param(
+            25,
+            [0, 0, 0, 1],
+            id=""
+        )
+    ]
+)
+def test_coim(amount_to_convert: int, extend_value: list) -> None:
+    assert get_coin_combination(amount_to_convert) == extend_value
